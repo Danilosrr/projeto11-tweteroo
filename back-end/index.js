1be4961913d29b2a2d99842c3da955c644ad4d4f
Danilo <danilo_srr@hotmail.com>
2022-04-23 13:30:01 -0300

    /tweets post and get added

--- back-end/index.js
+++ back-end/index.js
@@ -9,6 +9,8 @@ let user = {
   username: "",
   avatar: "",
 };
+let tweets = [];
+
 
 app.post('/sign-up', (req, res) => {
   const received = req.body;
@@ -16,8 +18,28 @@ app.post('/sign-up', (req, res) => {
   user.username = received.username,
   user.avatar = received.avatar
 
-  console.log(user);
-  res.status(200).send('Ok');
-})
+  res.status(201).send('Ok');
+});
 
-app.listen(5000)
+app.post('/tweets', (req, res) => {
+  const received = req.body;
+  let tweet = {
+    username: received.username,
+    avatar: user.avatar,
+    tweet:  received.tweet
+  };
+  tweets.push(tweet);
+  console.log(tweets);
+  res.status(201).send('Ok');
+});
+
+app.get('/tweets',(req, res) => {
+  if (tweets.length>=10){
+    tweets=tweets.slice(Math.max(tweets.length-10,0));
+    res.status(200).send(tweets);
+  }else{
+    res.status(200).send(tweets);  
+  }
+});
+
+app.listen(5000);
